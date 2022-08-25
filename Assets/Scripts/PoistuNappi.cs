55fa002056b9c650f560b6a40a85edafa54f53cf
leo lyytikäinen <leo.lyytikainen@student.careeria.fi>
2022-08-25 13:02:10 +0300

    mainmenu
    mainmenun lisäys. poistu nappi valmis

---
+++ Assets/Scripts/PoistuNappi.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ST
+{
+    public class PoistuNappi : MonoBehaviour
+    {
+        public void poistu()
+        {
+            Application.Quit();
+        }
+    }
+}
