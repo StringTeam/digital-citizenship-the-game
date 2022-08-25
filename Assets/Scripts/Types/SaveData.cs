0a75973ec7926f3f109ea92b85160bdd2de32d7a
Jonni Liljamo <jonni@liljamo.com>
2022-08-25 12:56:16 +0300

    Update SaveManager, update SaveData

---
+++ Assets/Scripts/Types/SaveData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ST.Types
+{
+    [System.Serializable]
+    public class SaveData
+    {
+        public string SaveName { get; set; }
+        public string PlayerName { get; set; }
+    }
+}
