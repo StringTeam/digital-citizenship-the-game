729db5512dfb0274f05faf805f1a5bff0fddb553
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2023-05-03 12:07:09 +0300

    Tips

---
+++ Assets/back.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class back : MonoBehaviour
+{
+    public string sceneName;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeScene);
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
